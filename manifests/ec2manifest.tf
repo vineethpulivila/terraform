ca1da2677ac9d425c96301f8e3a6be95c76cbeb9
vineethpulivila <vineethpulivila@outlook.com>
2024-05-29 19:54:12 +0530

    Update ec2manifest.tf

--- manifests/ec2manifest.tf
+++ manifests/ec2manifest.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 #### aws instance creation###
-resource "aws_instance" "ec2demo" {
+resource "aws_instance" "vm" {
     ami = "ami-00beae93a2d981137"
     instance_type = "t2.micro"
     tags = {
