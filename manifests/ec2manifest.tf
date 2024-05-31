2761940ac9343d97df6fd2dcb0721130ae889103
vineethpulivila <vineethpulivila@outlook.com>
2024-05-31 19:12:31 +0530

    test

--- manifests/ec2manifest.tf
+++ manifests/ec2manifest.tf
@@ -7,7 +7,7 @@ provider "aws" {
 #### aws instance creation###
 resource "aws_instance" "vm" {
     ami = "ami-00beae93a2d981137"
-    instance_type = "t2.micro"
+    instance_type = "t3.micro"
     tags = {
         Name = "amazoninstance"
     }
